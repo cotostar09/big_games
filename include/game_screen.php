5835b160dd688756dd2773f929f70ae3fc2cfc27
yongmin <cotostar09@gmail.com>
2018-04-14 02:01:42 +0900

    update index, add /include, /setting

---
+++ include/game_screen.php
@@ -0,0 +1,85 @@
+<?php
+
+?>
+<!-- main content start-->
+		<div id="page-wrapper">
+			<div class="main-page">
+			<div class="col_3">
+			<div class="col-md-3 widget widget1">
+        		<div class="r3_counter_box">
+                    <i class="pull-left fa fa-dollar icon-rounded"></i>
+                    <div class="stats">
+                      <h5><strong>ROUND 1</strong></h5>
+                      <span>08:20</span><br/>
+                      <span>.   </span>
+                    </div>
+                </div>
+        	</div>
+        	<div class="col-md-3 widget widget1">
+        		<div class="r3_counter_box">
+                    <i class="pull-left fa fa-dollar icon-rounded"></i>
+                    <div class="stats">
+                      <h5><strong>APACHE </strong></h5>
+                      <span>WINNER player - 문용민 / 서수영 / 박소엽</span><br/>
+                      <span>Total Point - 10,000</span>
+                    </div>
+                </div>
+        	</div>
+        	<div class="col-md-3 widget widget1">
+        		<div class="r3_counter_box">
+                    <i class="pull-left fa fa-laptop user1 icon-rounded"></i>
+                    <div class="stats">
+                      <h5><strong>PHP</strong></h5>
+                      <span>WINNER player - 문용민 / 서수영</span><br/>
+                      <span>Total Point - 10,000</span>
+                    </div>
+                </div>
+        	</div>
+        	<div class="col-md-3 widget widget1">
+        		<div class="r3_counter_box">
+                    <i class="pull-left fa fa-money user2 icon-rounded"></i>
+                    <div class="stats">
+                      <h5><strong>MySQL</strong></h5>
+                      <span>WINNER player - 문용민</span><br/>
+                      <span>Total Point - 10,000</span>
+                    </div>
+                </div>
+        	</div>
+        	<div class="col-md-3 widget widget1">
+        		<div class="r3_counter_box">
+                    <i class="pull-left fa fa-pie-chart dollar1 icon-rounded"></i>
+                    <div class="stats">
+                      <h5><strong>내 재정상태</strong></h5>
+                      
+                      <TABLE class="table">
+                      <TR>
+                      	<TD><span><B>POINT</B></span></TD>
+                      	<TD><span>100P</span></TD>
+                      	<TD><span><B>APACHE right</B></span></TD>
+                      	<TD><span>1</span></TD>
+                      </TR>
+                      <TR>
+                      	<TD><span><B>PHP right</B></span></TD>
+                      	<TD><span>0</span></TD>
+                      	<TD><span><B>MySQL right</B></span></TD>
+                      	<TD><span>0</span></TD>
+                      </TR>
+                      </TABLE>
+                    </div>
+                </div>
+        	 </div>
+        	<div class="clearfix"> </div>
+		</div>
+		
+
+			
+		</div>
+				
+			<!-- FOR HIGH CHARTS SCRIPTS -->	
+			<script src="js/highcharts.js"></script>
+			<script src="js/series-label.js"></script>
+			<script src="js/exporting.js"></script>
+			<!-- FOR HIGH CHARTS SCRIPTS -->
+				
+			</div>
+		</div>
